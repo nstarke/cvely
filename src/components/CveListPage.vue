c643285ac5f773a93c16016384622587cd74a5bf
nstarke <starke.nicholas@gmail.com>
2023-01-31 07:58:46 -0600

    CVE List page displaying data

--- src/components/CveListPage.vue
+++ src/components/CveListPage.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="hello">
       <h2>CVE List</h2>
+      <ul>
+          <li v-for="(item, idx) in cveList" v-bind:key="item.cveId">
+              {{ idx }} - {{ item.cveId }} found for term {{ item.term }} (Created {{ item.createdAt }})
+          </li>
+      </ul>
     </div>
   </template>
   
@@ -21,6 +26,9 @@
               self.cveList = cveList;
           })
       }
+    },
+    mounted() {
+        this.getList();
     }
   }
   </script>
