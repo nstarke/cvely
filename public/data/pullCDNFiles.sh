9f83827070e4d388a87362dd469ec9e38a4f3ce5
nstarke <starke.nicholas@gmail.com>
2023-02-02 17:22:25 -0600

    updating local CDN tools for importing

--- public/data/pullCDNFiles.sh
+++ public/data/pullCDNFiles.sh
@@ -4,4 +4,6 @@ do
     wget "https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$YEAR.json.gz"
 done
 gunzip *.gz
+echo "Creating Self Signed Certificate for local use only."
+openssl req -new -x509 -sha256 -keyout server.pem -out server.pem -days 365 -nodes
 exit
--- public/data/serveLocalCdn.py
+++ public/data/serveLocalCdn.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
+import ssl
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
-    def end_headers(self):
+   def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
         return super(CORSRequestHandler, self).end_headers()
 
-
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain('./server.pem')
 httpd = HTTPServer(('0.0.0.0', 8080), CORSRequestHandler)
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 httpd.serve_forever()
