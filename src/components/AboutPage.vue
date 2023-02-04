e3acccd680e418a667c1bb1a48c5b2dbe8a77a60
nstarke <starke.nicholas@gmail.com>
2023-02-04 06:37:50 -0600

    adding instructions and other information

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2>About</h2>
+    <p>CVE Feed was created by <a href="https://twitter.com/nstarke">Nicholas Starke</a></p>
   </div>
 </template>
 
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <p>CVE Feed is a way to manage CVE data</p>
+        <p>CVE Feed is a way for security researchers to manage CVE data.  It works by tracking new CVEs for user-defined keywords.</p>
+        <p>After you have defined keywords you are interested in seeing new CVEs for, you can sync those keywords against the NVD CVE database</p>
+        <p>You can also match your keywords against any historical date.</p>
+        <p>Optionally, you can import CVE Data from a given year (or all years!)</p>
+        <p>Historical CVE Data will not match against keywords when imported.</p>
     </div>
   </template>
   
